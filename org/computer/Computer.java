3ba4bb9cabfde872de8cc0ff6c6c8c42a0a61787
sharmi101 <sharmilamohan710@gmail.com>
2022-11-21 12:24:58 +0530

    send a push

---
+++ org/computer/Computer.java
@@ -0,0 +1,9 @@
+package org.computer;
+
+public class Computer {
+	public void computerModel() {
+		System.out.println("idea pad 3");
+
+	}
+
+}
---
+++ org/computer/Desktop.java
@@ -0,0 +1,16 @@
+package org.computer;
+
+public class Desktop extends Computer{
+	private void desktopSize() {
+		System.out.println("14");
+
+	}
+	public static void main(String[] args) {
+		Desktop d=new Desktop();
+		d.desktopSize();
+		d.computerModel();
+		
+		
+	}
+
+}
